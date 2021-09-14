05dd449748cd58cf2a5efecf7dc595eef7d347eb
Web-lycan3d <Web@lycan3d.com>
2021-09-14 18:10:13 +0530

    contact footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 /** @format */
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 
 import "./App.styles.scss";
-import Navbar from "./components/nav/Navbar";
-import Firstpage from "./pages/home/Firstpage";
 
-function App() {
+import Navbar from "./components/nav/Navbar";
+import Footer from './components/footer/footer.component'
+import Firstpage from "./pages/home/Firstpage";
+import Contact from "./pages/contact/contact.component"
+
+const App = () => {
   return (
-    <div>
+    <Fragment>
       <Navbar />
-      <Firstpage />
-    </div>
+      <Switch>
+        <Route exact path="/" component={Firstpage} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+      <Footer/>
+    </Fragment>
   );
 }
 
---
+++ client/src/pages/contact/contact.component.jsx
@@ -0,0 +1,45 @@
+import React, { Fragment } from 'react'
+import { IoMail, IoCall, IoLocation } from "react-icons/io5";
+
+import './contact.styles.scss'
+
+const Contact = () => {
+    return (
+        <Fragment>
+            <div className="contact-banner">
+                <img src="https://i.ibb.co/kqkwX9M/Group-5423.png" alt="Contact" />
+                <div className="contact-title">
+                    <h1>Contact us</h1>
+                    <h4>Lorem Ipsum Dolor Sit Amet, Consecteur Adipiscing Elit</h4>
+                </div>
+            </div>
+            <div className="contact-container">
+                <div className="contact-details">
+                    <div className="detail">
+                        <IoMail className="contact_icon" />
+                        <span>support@pyuraair.com</span>
+                    </div>
+                    <div className="detail">
+                        <IoCall className="contact_icon" />
+                        <span>+91 9591807979</span>
+                    </div>
+                    <div className="detail">
+                        <IoLocation className="contact_icon" />
+                        <span>IKP Eden, Kormangala, Bengaluru - 29</span>
+                    </div>
+                </div>
+                <div className="map">
+                    <img src="https://i.ibb.co/Ny6bbjx/Mask-Group-33.png" alt="Pyura location" />
+                </div>
+                <div className="contact-content">
+                    <p>
+                        Please feel free to reach out to us with any concerns or suggestions you might have about our product. As our valued customers,
+                         we would deeply appreciate your feedback so that we can constantly strive to improve while offering you the highest quality services and an overall fulfilling experience.
+                    </p>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Contact
