759d852307aa8dd350b63bc301ee8c6137a928f9
Web-lycan3d <Web@lycan3d.com>
2021-09-24 18:54:13 +0530

    config

--- server/routes/Verify.js
+++ server/routes/Verify.js
@@ -2,16 +2,17 @@
 
 const express = require("express");
 const router = express.Router();
+const config = require("config")
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = config.get("TWILIO_ACCOUNT_SID");
+const authToken = config.get("TWILIO_AUTH_TOKEN");
 
 const client = require("twilio")(accountSid, authToken);
 
 router.post("/user/phone", (req, res) => {
   const { phoneNo } = req.body;
   client.verify
-    .services(process.env.TWILIO_SID)
+    .services(config.get("TWILIO_SID"))
     .verifications.create({
       to: phoneNo,
       channel: "sms",
@@ -24,7 +25,7 @@ router.post("/user/otp", (req, res) => {
   console.log("S");
   const { phoneNo, userOtp } = req.body;
   client.verify
-    .services(process.env.TWILIO_SID)
+    .services(config.get("TWILIO_SID"))
     .verificationChecks.create({
       to: phoneNo,
       code: userOtp,
