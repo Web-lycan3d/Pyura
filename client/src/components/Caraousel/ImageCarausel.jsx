0282fa6775ad967799752ea25c8b5eddccff0c6c
Web-lycan3d <Web@lycan3d.com>
2021-09-29 14:42:53 +0530

    changed the renders

--- client/src/components/Caraousel/ImageCarausel.jsx
+++ client/src/components/Caraousel/ImageCarausel.jsx
@@ -1,40 +1,32 @@
 /** @format */
 
-import React, { useState } from "react";
+import React from "react";
 import "./carausel.styles.scss";
 
 import ImageGallery from "react-image-gallery";
-import { IoMdRadioButtonOn } from "react-icons/io";
-import { BiFullscreen } from "react-icons/bi";
 const ImageCarausel = () => {
   const images = [
     {
-      original: "https://i.ibb.co/5scjhzY/Pyura-renders-1.png",
-      thumbnail: "https://i.ibb.co/5scjhzY/Pyura-renders-1.png",
+      original: "https://i.ibb.co/8mktTPb/p1.webp",
+      thumbnail: "https://i.ibb.co/8mktTPb/p1.webp",
     },
     {
-      original: "https://i.ibb.co/r7hLtrd/Perspective-View-1.png",
-      thumbnail: "https://i.ibb.co/r7hLtrd/Perspective-View-1.png",
+      original: "https://i.ibb.co/YyBKxTR/p2.webp",
+      thumbnail: "https://i.ibb.co/YyBKxTR/p2.webp",
     },
     {
-      original: "https://i.ibb.co/nfwmg0B/Pyura-renders-7.png",
-      thumbnail: "https://i.ibb.co/nfwmg0B/Pyura-renders-7.png",
+      original: "https://i.ibb.co/2dB9ytk/p3.webp",
+      thumbnail: "https://i.ibb.co/2dB9ytk/p3.webp",
     },
     {
-      original: "https://i.ibb.co/CMQFNQk/Pyura-renders-2.png",
-      thumbnail: "https://i.ibb.co/CMQFNQk/Pyura-renders-2.png",
+      original: "https://i.ibb.co/Rct4pzF/p5.webp",
+      thumbnail: "https://i.ibb.co/Rct4pzF/p5.webp",
     },
     {
-      original: "https://i.ibb.co/WKbtMcw/Pyura-renders-2.webp",
-      thumbnail: "https://i.ibb.co/WKbtMcw/Pyura-renders-2.webp",
+      original: "https://i.ibb.co/NWMJdKk/Pyura-renders-6.webp",
+      thumbnail: "https://i.ibb.co/NWMJdKk/Pyura-renders-6.webp",
     },
   ];
-
-  const [value, setValue] = useState(1);
-
-  const onChange = (value) => {
-    setValue(value);
-  };
   return (
     <div>
       <ImageGallery
